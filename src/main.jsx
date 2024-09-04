9cececa7c738e8825fde18ce765c47877486e5c9
Victor Zornek <vzornek@gmail.com>
2024-09-04 17:03:34 -0300

    adicionando os arquivos de estilização e cores no projeto

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from './styles/global.js'
 
+import theme from './styles/theme.js'
 
 import { Details } from './pages/Details/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Details />
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Details />
+    </ThemeProvider>
   </StrictMode>,
 )
---
+++ src/styles/theme.js
@@ -0,0 +1,25 @@
+export default {
+    COLORS: {
+        GRAY_100: "#E5E5E5",
+        GRAY_200: "#CAC4CF",
+        GRAY_400: "#948F99",
+        GRAY_700: "#312E38",
+        
+        BACKGROUND_400: "#312E38",
+        BACKGROUND_700: "#262529",
+        BACKGROUND_900: "#1C1B1E",
+        
+        PINK: "#FF859B",
+        PINK_TRANSPARENT: "rgba(255, 133, 155, 0.05)",
+        
+        WHITE_500: "#F4EDE8",
+        WHITE_900: "#FFFFFF",
+
+        EBONY_CLAY: "#282124",
+
+        BACKGROUND_BLACK_700: "#0D0C0F",
+
+        BLACK_500: "#1C1B1E",
+        BLACK_900: "#000000"
+    }
+}
