176d074abfcab2ca2f34de6cbc44be9f5b1c92d5
Victor Zornek <vzornek@gmail.com>
2024-10-11 17:21:44 -0300

    alteração no header de signOut

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,18 @@
+import { useNavigate } from "react-router-dom";
 import { Container, Profile, Input } from "./styles";
 
 import { useAuth } from "../../hooks/auth";
 
 export function Header() {
     const { signOut } = useAuth();
+
+    const navigate = useNavigate()
+
+    function handleSignOut(event) {
+        event.preventDefault()
+        navigate("/")
+        signOut()
+    }
 
     return(
         <Container>
@@ -14,7 +23,7 @@ export function Header() {
             <Profile to='/profile'>
                 <div>
                     <strong>Victor Zornek</strong>
-                    <a onClick={signOut} >sair</a>
+                    <a onClick={handleSignOut} >sair</a>
                 </div>
 
                 <img src="https://github.com/VictorZornek.png" alt="Foto do usuário" />
